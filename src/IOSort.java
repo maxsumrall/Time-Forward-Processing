e8b992be67e9168351cb433363488953146891ab
Jonathan Sumrall <jmsumrall@gmail.com>
2014-03-13 16:37:38 +0100

    the idea is there, but has bugs.

--- src/IOSort.java
+++ src/IOSort.java
@@ -15,6 +15,7 @@ public class IOSort {
     FileChannel edgesFileChannel;
     int smallestSubsetSize = 8; //how many edges to sort in-memory. NUMBER OF EDGES!
     long edgesInFile;
+    long edgesBufferSize;
     IOSort(File edgesFile){
         //Open the file containing the edges for reading only.
         ByteBuffer tempBuffer = ByteBuffer.allocate(8);
@@ -24,7 +25,7 @@ public class IOSort {
             edgesFileChannel.read(tempBuffer);  //read the first number from the file
             tempBuffer.flip();
             int n = tempBuffer.getInt() + tempBuffer.getInt();   //remember the padding which makes the first one 8 bytes.
-            long edgesBufferSize =  (n*3*2*4)+8;
+            edgesBufferSize =  (n*3*2*4);
             this.edgesBuffer = this.edgesFileChannel.map(FileChannel.MapMode.READ_WRITE, 8, edgesBufferSize); //Prepared to handle huge number of edges without consuming heap space
         }
         catch (IOException e){e.printStackTrace();}
@@ -50,11 +51,31 @@ public class IOSort {
         }
     }
 
+    /**
+     * We have two buffers which are actual files but we can treat them like buffers in memory since the OS handles paging
+     *
+     * EDGES = buffer holding the edges in various states of unorderedness
+     * TEMP = buffer where merges go
+     *
+     * Edges are group into subsets. Initialy I set this as 8. Every set of 8 edges in the EDGES buffer are brought into memory and sorted naively.
+     * Then, we do mergesort starting with these subsets.
+     *
+     * P and Q represent the current active point within a subset while merging. Initially they refer to the first element in their respective subsets
+     *
+     * You compare the edge at P and the edge at Q. Take the smaller one and put it in TEMP (along with the second element, since an edge is 2 Ints but we compare only the first)
+     * Whichever one (P or Q) was smaller and was put in TEMP, increment P or Q so that the next comparison works.
+     * Continue until either P or Q reaches the end of its subsection, and put the remaining elements of the other subsection into TEMP
+     * Increment both P and Q to the next subsections.
+     * Repeat until all subsections have been merged into TEMP
+     * Swap EDGES and TEMP, double the size of the subsections, and repeat until the size of the subsections is the size of the full buffer.
+     * Make sure you get EDGES and TEMP swapped back or something.
+     */
     public void mergeSort(){
-        int subsetSize = smallestSubsetSize;
+
+        int subsetSize = smallestSubsetSize*8;
         int currentIndex = 0;
-        MappedByteBuffer P;//P and Q are the two sets I will merge[sort]
-        MappedByteBuffer Q;
+        int P;//P and Q are location indices into the two sets I will merge[sort]
+        int Q;
         //make two maps over the file channel, one for each subset which will be merged.
         // File = [<------P------>|<------Q------>|....................]
 
@@ -68,11 +89,14 @@ public class IOSort {
        }
         catch(IOException e){e.printStackTrace();return;}
         while(subsetSize < edgesInFile){
+            System.out.println(subsetSize/8);
                 while(currentIndex+ 2*subsetSize < (this.edgesInFile*8)){
                 try{
-                    B = tempFileChannel.map(FileChannel.MapMode.READ_WRITE,0,this.edgesBuffer.remaining());  //size of the original buffer
-                    P = this.edgesFileChannel.map(FileChannel.MapMode.READ_WRITE,currentIndex,currentIndex+subsetSize);
-                    Q = this.edgesFileChannel.map(FileChannel.MapMode.READ_WRITE,currentIndex+subsetSize, currentIndex + 2*subsetSize);
+                    this.edgesFileChannel.position(8);
+                    B = tempFileChannel.map(FileChannel.MapMode.READ_WRITE,0,this.edgesBufferSize);  //size of the original buffer
+                    //P = this.edgesFileChannel.map(FileChannel.MapMode.READ_WRITE,currentIndex,currentIndex+subsetSize);
+                    //Q = this.edgesFileChannel.map(FileChannel.MapMode.READ_WRITE,currentIndex+subsetSize, currentIndex + 2*subsetSize);
+
                 }
                 catch (IOException e){e.printStackTrace(); return;}
                 int x = P.getInt();
