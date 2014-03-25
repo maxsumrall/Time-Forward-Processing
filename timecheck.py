bc592af7420f96f1ee8abdda0ad13f925b7ff778
Jonathan Sumrall <jmsumrall@gmail.com>
2014-03-25 09:51:02 +0100

    added python file for testing run time of unix sort

--- timecheck.py
+++ timecheck.py
@@ -3,7 +3,7 @@ import time
 
 startTime = time.time()
 #subprocess.call("sort test25regular-edges -n -o \"trashsorted.txt\" -T /scratch2 ", shell=True)
-subprocess.call("sort test10Mregular-edges -n -o trash", shell=True)
-print "running time" 
+subprocess.call("sort /scratch2/randomgraphs/test10Mregular-edges -n -o trash", shell=True)
+print "running time:" 
 print time.time()-startTime
 subprocess.call("rm trash",shell=True)
