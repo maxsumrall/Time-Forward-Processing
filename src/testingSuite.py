4e3fa9fbadda60cf8224fd79def2569dd10ef194
Jonathan Sumrall <jmsumrall@gmail.com>
2014-03-30 22:55:25 +0200

    making it possible to not redo topo sorting each time

--- src/testingSuite.py
+++ src/testingSuite.py
@@ -7,7 +7,7 @@ mValues = [40000]
 outputFile = open("results.txt","w")
 
 for n in nValues:
-  result = subprocess.check_output("java -server -d64 Main " + str(n) + " " + str(mValues[0] + " b" + ), shell=True)
+  result = subprocess.check_output("java -server -d64 Main " + str(n) + " " + str(mValues[0]) + " b", shell=True)
   print str(n) + ": " + result
   outputFile.write("N = " + str(n) + ":" + result + "\n")
 
