c7441bdfa8d789f70d5e498edaf0821a730cbad8
Jonathan Sumrall <jmsumrall@gmail.com>
2014-04-04 13:49:09 +0200

    Rewrote Main class to be able to specify from the command line.
    Refactored code so that file names used/created throughout classes have meaning and not used the same everywhere.

--- src/testingSuite.py
+++ src/testingSuite.py
@@ -7,7 +7,7 @@ mValues = [10000,30000,50000]
 with open("results.txt","w") as outputFile:
     for n in zip(nValues,fileName):
         for m in mValues:
-            command = "java -server Main " + str(n[1]) + " " + str(n[1]) + " TFP_Exp " + str(m)
+            command = "java -server Main " + str(n[1]) + " " + str(n[0]) + " TFP_Exp " + str(m)
             print command
             result = subprocess.check_output(command, shell=True)
             print "\t " + result
