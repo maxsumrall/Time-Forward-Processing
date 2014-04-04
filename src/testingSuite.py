b2d26ee7ad1c3689b8a9f9b967afc487b05677c9
Jonathan Sumrall <jmsumrall@gmail.com>
2014-04-04 13:48:07 +0200

    Rewrote Main class to be able to specify from the command line.
    Refactored code so that file names used/created throughout classes have meaning and not used the same everywhere.

--- src/testingSuite.py
+++ src/testingSuite.py
@@ -1,13 +1,15 @@
 import subprocess
 
-nValues = [3000000,6000000,9000000,12000000,15000000]
-mValues = [10000]
+nValues = [1500000, 3000000, 6000000]
+fileName = ["randomEdges1M","randomEdges3M","randomEdges6M"]
+mValues = [10000,30000,50000]
 
-outputFile = open("results.txt","w")
+with open("results.txt","w") as outputFile:
+    for n in zip(nValues,fileName):
+        for m in mValues:
+            command = "java -server Main " + str(n[1]) + " " + str(n[1]) + " TFP_Exp " + str(m)
+            print command
+            result = subprocess.check_output(command, shell=True)
+            print "\t " + result
+            outputFile.write(command + "\t" + result + "\n")
 
-for n in nValues:
-  result = subprocess.check_output("java -server -d64 Main " + str(n) + " " + str(mValues[0]) + " b", shell=True)
-  print str(n) + ": " + result
-  outputFile.write("N = " + str(n) + ":" + result + "\n")
-
-outputFile.close()
